d30ccb10720aa2a1f1acb12c38756b36db337670
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-10-28 13:04:48 +0100

    Add assertion about segments not intersecting

--- lab3/domowe/zad1.py
+++ lab3/domowe/zad1.py
@@ -1,22 +1,22 @@
-def get_common_section(section1, section2):
-    a1, b1 = section1
-    a2, b2 = section2
+def get_common_section(segment1, segment2):
+    a1, b1 = segment1
+    a2, b2 = segment2
 
-    # section has to be in a form (a, b) where a <= b
+    # segment has to be in a form (a, b) where a <= b
     assert a1 <= b1
     assert a2 <= b2
 
     are_intersecting = b1 >= a2 and b2 >= a1
+    assert are_intersecting, "given segments are not intersecting!"
 
-    left_boundary = max(a1, a2) if are_intersecting else -1
-    right_boundary = min(b1, b2) if are_intersecting else -1
+    left_boundary = max(a1, a2)
+    right_boundary = min(b1, b2)
 
     return (left_boundary, right_boundary)
 
 
-print(get_common_section((10, 12), (13, 15)))
 print(get_common_section((10, 15), (12, 16)))
 print(get_common_section((10, 12), (12, 15)))
 print(get_common_section((1, 1), (1, 1)))
-print(get_common_section((12, 15), (1, 2)))
 print(get_common_section((1, 2), (2, 3)))
+print(get_common_section((1,5), (7, 10)))
