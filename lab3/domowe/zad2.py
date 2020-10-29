2b71aac37bbe1414d9e0ef01ecb935a52e380eef
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-10-29 14:19:10 +0100

    fix flake warnings

--- lab3/domowe/zad2.py
+++ lab3/domowe/zad2.py
@@ -26,7 +26,8 @@ def print_text_row(width, frame_char, text):
 
 
 def print_string_in_frame(text, width, frame_char='*'):
-    assert width >= len(text) + 2, "given width is too small to render frame correctly!"
+    assert width >= len(
+        text) + 2, "given width is too small to render frame correctly!"
 
     print_frame_row(width, frame_char)
     print_text_padding_row(width, frame_char)
--- lab3/domowe/zad3.py
+++ lab3/domowe/zad3.py
@@ -2,7 +2,7 @@ def fill_rectange_with_char(height, width, fill_char='#'):
     for _ in range(height):
         for _ in range(width):
             print(fill_char, sep='', end='')
-        print() # newline
+        print()  # newline
 
 
 fill_rectange_with_char(3, 6)
