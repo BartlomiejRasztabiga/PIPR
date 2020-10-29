e035e6e728f9e177d96eff71a40be521aed81f5f
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-10-29 17:32:02 +0100

    small refactor

--- lab4/z_filmu/paragony.py
+++ lab4/z_filmu/paragony.py
@@ -35,6 +35,13 @@ def format_price(price):
     return f"{price_zl}.{price_gr:02}"
 
 
+def print_product(product, current_position):
+    name, price = product
+    price = format_price(price)
+    tax_group = get_tax_group(name)
+    print(f"{current_position:2}. {name:20}{price:>6}{tax_group:>2}")
+
+
 def print_receipt(date, receipt):
     """
     1. output date
@@ -49,23 +56,21 @@ def print_receipt(date, receipt):
     print(date)
     current_position = 1
     for name, price in receipt:
-        price = format_price(price)
-        print(f"{current_position:2}. {name:20}{price:>6}")
+        print_product((name, price), current_position)
         current_position += 1
-    print('-' * 30)
+    print('-' * 32)
     total_value = get_total_price(receipt)
     formatted_value = format_price(total_value)
     print(f"TOTAL: {formatted_value:>23}")
 
 
-def get_tax_group(product):
+def get_tax_group(product_name):
     tax_group_A = {'Milk', 'Bread'}
     tax_group_B = {'Bananas', 'Oranges'}
 
-    product_name, _ = product
-    if product in tax_group_A:
+    if product_name in tax_group_A:
         return 'A'
-    elif product in tax_group_B:
+    elif product_name in tax_group_B:
         return 'B'
     return 'E'
 
@@ -73,7 +78,8 @@ def get_tax_group(product):
 my_receipt = [
     ("Bananas", 49999),
     ("Oranges", 803),
-    ("Milk", 3155)
+    ("Milk", 3155),
+    ("Lollipop", 100)
 ]
 
 print_receipt(str(date.today()), my_receipt)
