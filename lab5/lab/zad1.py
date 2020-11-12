96e74e9f207899a02b9be122cf12c4eb50d6cd42
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-12 20:03:19 +0100

    Add alternative for lab5 zad1 just for fun

--- lab5/lab/zad1.py
+++ lab5/lab/zad1.py
@@ -2,4 +2,9 @@ def generate_fuzz_list(n):
     return ["fuzz" if x % 3 == 0 else x for x in range(1, n+1)]
 
 
+def generate_fuzz_list_map(n):
+    return list(map(lambda x: "fuzz" if x % 3 == 0 else x, range(1, n+1)))
+
+
 print(generate_fuzz_list(12))
+print(generate_fuzz_list_map(12))
