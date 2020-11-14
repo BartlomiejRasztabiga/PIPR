5934dcfbbb07fc6754c648947fcf1cb2dfee5eb7
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-14 10:39:23 +0100

    Add distance for non going around

--- lab6/przed/zad2.py
+++ lab6/przed/zad2.py
@@ -1,2 +1,5 @@
-def distance(a, b, c):
-    pass
+def distance(n, point1, point2):
+    point1_x, point1_y = point1
+    point2_x, point2_y = point2
+
+    return abs(point1_x - point2_x) + abs(point1_y - point2_y)
