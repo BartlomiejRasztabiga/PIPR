f9fe3f47d6584d0ccd41da3d7965f0ad225abd04
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-19 11:23:06 +0100

    lab6 pd zad3 INIT

---
+++ lab6/domowe/zad3.py
@@ -0,0 +1,40 @@
+def get_lab_points(lab_score):
+    pass
+
+
+def get_sum_of_lab(student_score):
+    pass
+
+
+def get_student_avg_lab_percentage(student_score, max_lab_points):
+    pass
+
+
+def get_single_student_summary(student_score, max_lab_points):
+    pass
+
+
+def get_all_students_summary(students_scores, max_lab_points):
+    pass
+
+
+def get_all_students_avg_lab_score(students_scores):
+    pass
+
+
+def print_all_students_summary(students_scores, max_lab_points):
+    pass
+
+
+def print_all_students_avg_lab_score(students_scores):
+    pass
+
+
+max_lab = [10, 20, 30]
+dane = [
+    ("Adam Abacki", [5, 10, 15]),
+    ("Basia Babacka", [10, 20, 30]),
+    ("Cecylia Cabacka", [1, 2, 3])
+]
+
+get_all_students_summary(dane, max_lab)
