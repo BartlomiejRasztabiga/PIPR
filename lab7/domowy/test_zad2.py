3dfc82387270f7b8fd6da4772353501c73415be2
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-26 23:19:25 +0100

    lab7 PD nearly done

--- lab7/domowy/test_zad2.py
+++ lab7/domowy/test_zad2.py
@@ -1,1 +1,124 @@
-from zad2 import *
+import pytest
+from zad2 import Route, RoutePoint, Stop, Time, InvalidTimeException
+
+
+def __prepare_simple_route_points():
+    return [
+        RoutePoint(Stop('stop 1', False, Time(0, 0)), Time(0, 0)),
+        RoutePoint(Stop('stop 2', False, Time(0, 0)), Time(0, 1)),
+        RoutePoint(Stop('stop 3', False, Time(0, 0)), Time(0, 2))
+    ]
+
+
+def __prepare_complex_route_points():
+    return [
+        RoutePoint(Stop('stop 1', False, Time(0, 1)), Time(0, 0)),
+        RoutePoint(Stop('stop 2', False, Time(0, 1)), Time(0, 3)),
+        RoutePoint(Stop('stop 3', True, Time(0, 5)), Time(0, 4)),
+        RoutePoint(Stop('stop 4', True, Time(0, 1)), Time(0, 7)),
+        RoutePoint(Stop('stop 5', False, Time(0, 1)), Time(0, 2))
+    ]
+
+
+def test_time_create_wrong_value_hours1():
+    with pytest.raises(InvalidTimeException):
+        Time(25, 0)
+
+
+def test_time_create_wrong_value_hours2():
+    with pytest.raises(InvalidTimeException):
+        Time(-25, 0)
+
+
+def test_time_create_wrong_value_minutes_1():
+    with pytest.raises(InvalidTimeException):
+        Time(0, 65)
+
+
+def test_time_create_wrong_value_minutes_2():
+    with pytest.raises(InvalidTimeException):
+        Time(0, -65)
+
+
+def test_time_add_simple():
+    time1 = Time(15, 30)
+    time2 = Time(0, 15)
+    assert time1 + time2 == Time(15, 45)
+
+
+def test_time_add_oclock():
+    time1 = Time(15, 30)
+    time2 = Time(0, 30)
+    assert time1 + time2 == Time(16, 00)
+
+
+def test_time_add_with_carry():
+    time1 = Time(15, 30)
+    time2 = Time(0, 46)
+    assert time1 + time2 == Time(16, 16)
+
+
+def test_get_route_point_description_without_change():
+    route_point = RoutePoint(Stop('stop 1', False, Time(0, 1)), Time(0, 5))
+    route_point_description = route_point.get_route_point_description(Time(13, 50))
+    assert route_point_description == "arrival: 13:50  departure: 13:51 (stop 1) - CHANGE_NOT_POSSIBLE"
+
+
+def test_get_route_point_description_with_change():
+    route_point = RoutePoint(Stop('stop 1', True, Time(0, 1)), Time(0, 5))
+    route_point_description = route_point.get_route_point_description(Time(13, 50))
+    assert route_point_description == "arrival: 13:50  departure: 13:51 (stop 1) - CHANGE_POSSIBLE"
+
+
+def test_calculate_route_duration_without_wait_times():
+    route_points = __prepare_simple_route_points()
+
+    route = Route('A', route_points)
+    route_duration = route._calculate_route_duration()
+    assert route_duration == Time(0, 3)
+
+
+def test_calculate_route_duration_with_wait_times():
+    route_points = __prepare_complex_route_points()
+
+    route = Route('B', route_points)
+    route_duration = route._calculate_route_duration()
+    assert route_duration == Time(0, 25)
+
+
+def test_calculate_route_end_time():
+    route_points = __prepare_simple_route_points()
+
+    route = Route('A', route_points)
+    route_end_time = route._calculate_route_end_time(Time(13, 50))
+    assert route_end_time == Time(13, 53)
+
+
+def test_generate_timetable_simple_without_wait_times_without_changes():
+    route_points = __prepare_simple_route_points()
+
+    route = Route('A', route_points)
+    timetable = route.generate_timetable(Time(1, 5))
+    assert timetable == [
+        "Route A with 3 stops",
+        "1:05 -> 1:08",
+        "arrival: 1:05  departure: 1:05 (stop 1) - CHANGE_NOT_POSSIBLE",
+        "arrival: 1:06  departure: 1:06 (stop 2) - CHANGE_NOT_POSSIBLE",
+        "arrival: 1:08  departure: 1:08 (stop 3) - CHANGE_NOT_POSSIBLE"
+    ]
+
+
+def test_generate_timetable_complex():
+    route_points = __prepare_complex_route_points()
+
+    route = Route('B', route_points)
+    timetable = route.generate_timetable(Time(1, 5))
+    assert timetable == [
+        "Route B with 5 stops",
+        "1:05 -> 1:30",
+        "arrival: 1:06  departure: 1:07 (stop 1) - CHANGE_NOT_POSSIBLE",
+        "arrival: 1:10  departure: 1:11 (stop 2) - CHANGE_NOT_POSSIBLE",
+        "arrival: 1:19  departure: 1:24 (stop 3) - CHANGE_POSSIBLE",
+        "arrival: 1:27  departure: 1:28 (stop 4) - CHANGE_POSSIBLE",
+        "arrival: 1:30  departure: 1:31 (stop 5) - CHANGE_NOT_POSSIBLE"
+    ]
--- lab7/domowy/zad2.py
+++ lab7/domowy/zad2.py
@@ -1,8 +1,107 @@
+from dataclasses import dataclass
+
+from typing import List
+
+
+@dataclass()
+class InvalidTimeException(Exception):
+    message: str
+
+
+@dataclass()
+class Time:
+    hours: int
+    minutes: int
+
+    def __post_init__(self):
+        if self.hours > 24 or self.hours < 0:
+            raise InvalidTimeException("Hours are in invalid range")
+        elif self.minutes > 60 or self.minutes < 0:
+            raise InvalidTimeException("Minutes are in invalid range")
+
+    def __add__(self, other):
+        temp = Time(self.hours, self.minutes)
+        temp.hours += other.hours
+        temp.minutes += other.minutes
+
+        if temp.minutes >= 60:
+            temp.hours += temp.minutes // 60
+            temp.minutes -= 60
+        return temp
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+
+    def __str__(self):
+        return f"{self.hours}:{self.minutes:02}"
+
+
+@dataclass()
 class Stop:
-    self __init__()
+    name: str
+    is_possibility_to_change: bool
+    wait_time: Time
 
-class Route:
-    pass
+    def get_stop_description(self, time_of_arrival: Time) -> str:
+        time_of_departure = time_of_arrival + self.wait_time
+        possibility_of_change_suffix = f"CHANGE{'_NOT' if not self.is_possibility_to_change else ''}_POSSIBLE"
+        return f"arrival: {time_of_arrival}  departure: {time_of_departure} ({self.name}) - {possibility_of_change_suffix}"
 
+
+@dataclass()
 class RoutePoint:
-    pass
+    stop: Stop
+    time_of_travel_since_last_stop: Time
+
+    def get_route_point_description(self, time_of_arrival: Time) -> str:
+        return self.stop.get_stop_description(time_of_arrival)
+
+    def get_departure_delay(self):
+        return self.stop.wait_time + self.time_of_travel_since_last_stop
+
+
+@dataclass()
+class Route:
+    name: str
+    route_points: List[RoutePoint]
+
+    def _calculate_route_duration(self) -> Time:
+        return sum(point.get_departure_delay() for point in self.route_points)
+
+    def _calculate_route_end_time(self, start_time: Time) -> Time:
+        route_duration = self._calculate_route_duration()
+        return start_time + route_duration
+
+    def _get_route_header_line(self) -> str:
+        number_of_route_points = len(self.route_points)
+        return f"Route {self.name} with {number_of_route_points} stops"
+
+    def _get_route_start_end_times_line(self, start_time: Time):
+        end_time = self._calculate_route_end_time(start_time)
+        return f"{start_time} -> {end_time}"
+
+    def _get_route_points_descriptions(self, start_time: Time):
+        descriptions = []
+        time_of_arrival = start_time
+
+        for route_point in self.route_points:
+            time_of_arrival += route_point.get_departure_delay()
+            description = route_point.get_route_point_description(time_of_arrival)
+            descriptions.append(description)
+
+        return descriptions
+
+    def generate_timetable(self, start_time: Time):
+        timetable = [
+            self._get_route_header_line(),
+            self._get_route_start_end_times_line(start_time),
+            *self._get_route_points_descriptions(start_time)
+        ]
+
+        for row in timetable:
+            print(row)
+
+        return timetable
