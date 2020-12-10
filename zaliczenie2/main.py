1ea35bf3ba4a77d740680caeae26e004c8842b57
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-10 09:08:26 +0100

    Add warehouse report

--- zaliczenie2/main.py
+++ zaliczenie2/main.py
@@ -1,3 +1,7 @@
+import os
+from typing import List
+
+
 class UnknownUnitException(Exception):
     def __init__(self, unit):
         self.unit = unit
@@ -8,7 +12,7 @@ class UnitConversionException(Exception):
 
 
 class Amount:
-    def __init__(self, amount, unit_name):
+    def __init__(self, amount: int, unit_name: str):
         self.amount = amount
         self.unit_name = unit_name
 
@@ -30,9 +34,23 @@ class Amount:
     def amount(self, amount):
         self._amount = amount
 
-    def convert_to(self, another_unit_name):
+    def __add__(self, other):
+        if self.unit_name != other.unit_name:
+            raise ValueError('Cannot add two amounts with different names')
+
+        temp = Amount(self.amount, self.unit_name)
+        temp.amount += other.amount
+        return temp
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.unit_name == other.unit_name
+
+    def convert_to(self, another_unit_name: str, persist=True):
         if self.unit_name == another_unit_name:
-            raise UnitConversionException()
+            # no conversion
+            if not persist:
+                return Amount(self.amount, another_unit_name)
+            return
 
         self._validate_unit_name(another_unit_name)
 
@@ -42,8 +60,11 @@ class Amount:
         elif another_unit_name == 'ml' and self.unit_name == 'l':
             new_amount *= 1000
 
-        self.amount = new_amount
-        self.unit_name = another_unit_name
+        if persist:
+            self.amount = new_amount
+            self.unit_name = another_unit_name
+        else:
+            return Amount(new_amount, another_unit_name)
 
     @staticmethod
     def _validate_unit_name(unit_name):
@@ -52,10 +73,9 @@ class Amount:
 
 
 class Resource:
-    def __init__(self, name, amount, unit='l'):
+    def __init__(self, name: str, amount: Amount):
         self.name = name
         self.amount = amount
-        self.unit = unit
 
     @property
     def name(self):
@@ -74,9 +94,40 @@ class Resource:
         self._amount = amount
 
 
+class Warehouse:
+    def __init__(self):
+        self._resources = []
+
+    def add_resource(self, resource: Resource):
+        for _resource in self.resources:
+            if _resource.name == resource.name:
+                _resource.amount += resource.amount
+                return
+
+        self._resources.append(resource)
+
+    @property
+    def resources(self):
+        return self._resources
+
+    def get_warehouse_report(self):
+        result = [
+            f"| {'Zasób':<10} | {'Ilość w magazynie [l]':>21} |",
+            f"| ---------- | --------------------- |"
+        ]
+        for resource in self.resources:
+            amount_in_l = resource.amount.convert_to('l', persist=False).amount
+            result.append(f"| {resource.name:<10} | {amount_in_l:>21} |")
+        return result
+
+    def print_warehouse_report(self):
+        print(os.linesep.join(self.get_warehouse_report()))
+
+
 class Product:
-    def __init__(self, name):
+    def __init__(self, name, requirements: List[Resource]):
         self.name = name
+        self.requirements = requirements
 
     @property
     def name(self):
@@ -85,3 +136,19 @@ class Product:
     @name.setter
     def name(self, name):
         self._name = name
+
+    @property
+    def requirements(self):
+        return self._requirements
+
+    @requirements.setter
+    def requirements(self, requirements):
+        self._requirements = requirements
+
+
+if __name__ == "__main__":
+    warehouse = Warehouse()
+    warehouse.add_resource(Resource('gliceryna', Amount(20, 'l')))
+    warehouse.add_resource(Resource('aloes', Amount(15, 'l')))
+    warehouse.add_resource(Resource('alkohol', Amount(1500, 'ml')))
+    warehouse.print_warehouse_report()
--- zaliczenie2/test_main.py
+++ zaliczenie2/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from main import Amount, UnknownUnitException, UnitConversionException
+from main import Amount, UnknownUnitException, Warehouse, Resource, Product
 
 
 def test_wrong_unit():
@@ -10,6 +10,19 @@ def test_wrong_unit():
 
 def test_good_unit():
     assert Amount(1000, 'l')
+
+
+def test_add_amounts():
+    amount1 = Amount(1000, 'ml')
+    amount2 = Amount(2000, 'ml')
+    assert amount1 + amount2 == Amount(3000, 'ml')
+
+
+def test_add_amounts_different_names():
+    amount1 = Amount(1000, 'ml')
+    amount2 = Amount(2000, 'l')
+    with pytest.raises(ValueError):
+        amount1 + amount2
 
 
 def test_conversion_to_l():
@@ -28,15 +41,65 @@ def test_conversion_to_ml():
     assert amount.unit_name == 'l'
 
 
-def test_wrong_conversion():
+def test_conversion_same_name():
     amount = Amount(2500, 'ml')
+    amount.convert_to('ml')
 
-    with pytest.raises(UnitConversionException):
-        amount.convert_to('ml')
+    assert amount.amount == 2500
+    assert amount.unit_name == 'ml'
 
 
-def test_wrong_conversion2():
+def test_conversion_no_persistence():
+    amount = Amount(2500, 'ml')
+    new_amount = amount.convert_to('l', persist=False)
+
+    assert amount.amount == 2500
+    assert amount.unit_name == 'ml'
+
+    assert new_amount.amount == 2.5
+    assert new_amount.unit_name == 'l'
+
+
+def test_wrong_conversion():
     amount = Amount(2500, 'ml')
 
     with pytest.raises(UnknownUnitException):
         amount.convert_to('kg')
+
+
+def test_create_warehouse():
+    warehouse = Warehouse()
+    warehouse.add_resource(Resource('gliceryna', Amount(20, 'l')))
+    warehouse.add_resource(Resource('aloes', Amount(15, 'l')))
+    assert len(warehouse.resources) == 2
+
+
+def test_add_to_warehouse_duplicate():
+    warehouse = Warehouse()
+    warehouse.add_resource(Resource('gliceryna', Amount(20, 'l')))
+    warehouse.add_resource(Resource('gliceryna', Amount(15, 'l')))
+    assert len(warehouse.resources) == 1
+    assert warehouse.resources[0].name == 'gliceryna'
+    assert warehouse.resources[0].amount == Amount(35, 'l')
+
+
+def test_create_product_with_requirements():
+    requirements = [
+        Resource('alkohol', Amount(600, 'ml')),
+        Resource('gliceryna', Amount(150, 'ml')),
+        Resource('aloes', Amount(350, 'ml')),
+    ]
+    product = Product('aloesowy żel do dezynfekcji rąk', requirements)
+
+    assert len(product.requirements) == 3
+
+
+def test_get_warehouse_report():
+    warehouse = Warehouse()
+    warehouse.add_resource(Resource('gliceryna', Amount(20, 'l')))
+    warehouse.add_resource(Resource('aloes', Amount(15, 'l')))
+    warehouse.add_resource(Resource('alkohol', Amount(1500, 'ml')))
+    report = warehouse.get_warehouse_report()
+
+    assert report
+    print(report)
